a29550dd80bb3a48bc96095fcc9eaa324596a68b
Simon Hareter <simon.hareter@live.at>
2026-04-08 22:19:53 +0200

    finished flood fill algo. TODO: implement end game if player won and test cursor movement for bugs

--- src/Game.java
+++ src/Game.java
@@ -113,49 +113,108 @@ public class Game {
             renderGameCursorCoordinates();
             int key = readKey();
 
+            Direction dir = getDirection(key);
+            if(dir != null) handleMovement(dir);
+            
             switch (key) {
                 case 'q', -1, 3 -> {
                     // move TermCursor down to not override game prints
                     moveTermCursor(Direction.DOWN, rows + 3 - gameRowCursorPos);
                     quitGame();
                 }
-
                 case 'm' -> moveToMenu();
-
                 case 'r' -> resetGame();
-
-                case 'w', 'A', 'k' -> {
-                    if (gameRowCursorPos >= 1) {
-                        moveToNextHiddenField(Direction.UP);
-                    }
-                }
-
-                case 's', 'B', 'j' -> {
-                    if (gameRowCursorPos < rows - 1) {
-                        moveToNextHiddenField(Direction.DOWN);
-                    }
-                }
-
-                case 'a', 'D', 'h' -> {
-                    if (gameColCursorPos >= 1) {
-                        moveToNextHiddenField(Direction.LEFT);
-                    }
-                }
-
-                case 'd', 'C', 'l' -> {
-                    if (gameColCursorPos < cols - 1) {
-                        moveToNextHiddenField(Direction.RIGHT);
-                    }
-                }
-
                 case 'f' -> {
                     placeFlag();
                     renderMinesRemaining();
                 }
-
                 case '\r', '\n' -> revealField();
             }
         }
+    }
+    
+    private Direction getDirection(int key) {
+        return switch (key) {
+            case 'w', 'A', 'k' -> Direction.UP;
+            case 's', 'B', 'j' -> Direction.DOWN;
+            case 'a', 'D', 'h' -> Direction.LEFT;
+            case 'd', 'C', 'l' -> Direction.RIGHT;
+            default -> null;
+        };
+    }
+
+    private void handleMovement(Direction dir) {
+        if(hasAnyValidMove()) moveToNextHiddenField(dir);
+        else {
+            // find the next possible field depending on directional input
+            findNearestPossibleField(dir);
+        }
+    }
+
+    private boolean hasAnyValidMove() {
+        return stepsToNextHiddenField(Direction.UP) > 0 ||
+                stepsToNextHiddenField(Direction.DOWN) > 0 ||
+                stepsToNextHiddenField(Direction.LEFT) > 0 ||
+                stepsToNextHiddenField(Direction.RIGHT) > 0;
+    }
+
+    private boolean canMove(Direction dir) {
+        return switch (dir) {
+            case UP -> gameRowCursorPos >= 1;
+            case DOWN -> gameRowCursorPos < rows - 1;
+            case LEFT -> gameColCursorPos >= 1;
+            case RIGHT -> gameColCursorPos < cols - 1;
+        };
+    }
+
+    private void findNearestPossibleField(Direction dir) {
+        int bestScore = Integer.MAX_VALUE;
+        int nearestRow = -1;
+        int nearestCol = -1;
+        for(int row = 0; row < rows; row++) {
+            for(int col = 0; col < cols; col++) {
+                if(map[row][col].isVisible()) continue;
+                int distance = Math.abs(row - gameRowCursorPos) + Math.abs(col - gameColCursorPos);
+                int penalty = calcPenalty(dir, row, col);
+                int score = distance + penalty;
+                if(score < bestScore) {
+                    bestScore = score;
+                    nearestRow = row;
+                    nearestCol = col;
+                }
+            }
+        }
+
+        if(nearestRow != -1) {
+            IO.print("\033[s");
+            moveTermCursor(Direction.DOWN, 13);
+            IO.println(nearestRow + " / " + nearestCol);
+            IO.print("\033[u");
+            moveCursorTo(nearestRow, nearestCol);
+            gameRowCursorPos = nearestRow;
+            gameColCursorPos = nearestCol;
+        }
+    }
+
+    private int calcPenalty(Direction dir, int row, int col) {
+        int penalty = 0;
+
+        switch (dir) {
+            case DOWN -> {
+                if (row <= gameRowCursorPos) penalty += 1000;
+            }
+            case UP -> {
+                if (row >= gameRowCursorPos) penalty += 1000;
+            }
+            case RIGHT -> {
+                if (col <= gameColCursorPos) penalty += 1000;
+            }
+            case LEFT -> {
+                if (col >= gameColCursorPos) penalty += 1000;
+            }
+        }
+
+        return penalty;
     }
 
     private void quitGame() {
@@ -258,21 +317,11 @@ public class Game {
         timerThread.start();
     }
 
-    private void resetTimer() {
-        isGameRunning = false;
-        if (timerThread != null) {
-            timerThread.interrupt();
-        }
-
-        secondsElapsed.set(0);
-        isGameRunning = true;
-    }
-
     private void renderTimer() {
         IO.print("\033[s"); // save cursor pos
         moveTermCursor(Direction.UP, GAME_TOP_UI_ROWS + gameRowCursorPos);
-        moveTermCursor(Direction.RIGHT, COL_WIDTH * (cols - gameColCursorPos) - 1);
-        System.out.printf("%03d", secondsElapsed.get());
+        moveTermCursor(Direction.RIGHT, COL_WIDTH * (cols - gameColCursorPos) - 2);
+        System.out.printf("%04d", secondsElapsed.get());
         IO.print("\033[u"); // resume to saved cursor pos
     }
 
@@ -441,7 +490,6 @@ public class Game {
     private void revealField() {
         Field f = map[gameRowCursorPos][gameColCursorPos];
         if (f.isFlagged()) return;
-        f.setVisible(true);
 
         int minesNearby = f.getAdjacentMines();
 
@@ -458,50 +506,45 @@ public class Game {
         }
 
         if(minesNearby == 0) {
-            revealEmptyRegion();
+            revealEmptyRegion(gameRowCursorPos, gameColCursorPos);
             return;
         }
+
+        f.setVisible(true);
 
         IO.print(getColoredMineCount(minesNearby));
         moveTermCursor(Direction.LEFT, 1);
     }
 
-    private void revealEmptyRegion() {
-        IO.print(getColoredMineCount(0));
+    private void revealEmptyRegion(int row, int col) {
+        if(isOutOfBounds(row, col)) return;
+
+        Field currField = map[row][col];
+
+        if(currField.isVisible() || currField.isMine()) return;
+
+        currField.setVisible(true);
+
+        int minesNearby = currField.getAdjacentMines();
+
+        IO.print("\033[s");
+        moveCursorTo(row, col);
+        IO.print(getColoredMineCount(currField.getAdjacentMines()));
         moveTermCursor(Direction.LEFT, 1);
-        // need to design an algorithm that finds all adjacent empty fields till each direction encounters a field non-zero.
+        IO.print("\033[u");
 
-        // 1. Check neighbors and reveal if empty
-        revealSurroundingFields();
+        if (minesNearby != 0) return;
 
-    }
-
-    private void revealSurroundingFields() {
-        for (int row = -1; row < 2; row++) {
-            for (int col = -1; col < 2; col++) {
-                if (row == 0 && col == 0) continue;
-
-                int tempRow = gameRowCursorPos + row;
-                int tempCol = gameColCursorPos + col;
-
-                if(isOutOfBounds(tempRow, tempCol)) continue;
-
-                Field currField = map[tempRow][tempCol];
-
-                IO.print("\033[s");
-
-                moveCursorTo(tempRow, tempCol);
-                currField.setVisible(true);
-                IO.print(getColoredMineCount(currField.getAdjacentMines()));
-                moveTermCursor(Direction.LEFT, 1);
-
-                IO.print("\033[u");
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                revealEmptyRegion(row + i, col + j);
             }
         }
     }
 
     private void moveCursorTo(int row, int col) {
-        // calculate how many fields the cursor has to move to reach destination map[row][col]
+        // calculate how many fields the cursor has to move to reach destination: map[row][col]
         int moveRow = row - gameRowCursorPos;
         int moveCol = col - gameColCursorPos;
 
@@ -522,6 +565,21 @@ public class Game {
 
         map[row][col].setMine(true);
         map[gameRowCursorPos][gameColCursorPos].setMine(false);
+
+        updateNeighbours(row, col, 1);
+        updateNeighbours(gameRowCursorPos, gameColCursorPos, -1);
+    }
+
+    private void updateNeighbours(int row, int col, int adjacentMineChange) {
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(isOutOfBounds(row + i, col + j)) continue;
+                if(i == 0 && j == 0) continue;
+
+                int adjacentMines = map[row + i][col + j].getAdjacentMines();
+                map[row + i][col + j].setAdjacentMines(adjacentMines + adjacentMineChange);
+            }
+        }
     }
 
     private void moveToMenu() {
@@ -543,7 +601,6 @@ public class Game {
         if(gameColCursorPos > 0 ) moveTermCursor(Direction.LEFT, gameColCursorPos * COL_WIDTH);
         gameRowCursorPos = 0;
         gameColCursorPos = 0;
-
 
         // show all mines
         for(int row = 0; row < rows; row++) {
@@ -581,21 +638,43 @@ public class Game {
                 case 'q', 3 -> {
                     moveTermCursor(Direction.DOWN, rows + 3 - gameRowCursorPos);
                     IO.print("\033[1G");    // move cursor to first column
-                    IO.print("\033[?25h");  // show cursor
-                    disableRawMode();
-                    System.exit(0);
+                   quitGame();
                 }
             }
         }
     }
 
     private void resetGame() {
+        resetFlagCounter();
+        resetCursorPosition();
+        resetTimer();
+        playGame();
+    }
+
+    private void resetFlagCounter() {
+        IO.print("\033[s");
+        moveTermCursor(Direction.UP, GAME_TOP_UI_ROWS  + gameRowCursorPos);
+        IO.print("\033[1G");
+        minesRemaining = difficulty.getMines();
+        IO.print(minesRemaining);
+        moveTermCursor(Direction.LEFT, 1);
+        IO.print("\033[u");
+    }
+
+    private void resetCursorPosition() {
         int GAME_LEFT_UI_COLS = 3;
         moveTermCursor(Direction.LEFT, gameColCursorPos * COL_WIDTH + GAME_LEFT_UI_COLS);
         moveTermCursor(Direction.UP, gameRowCursorPos + GAME_TOP_UI_ROWS);
+    }
 
-        resetTimer();
-        playGame();
+    private void resetTimer() {
+        isGameRunning = false;
+        if (timerThread != null) {
+            timerThread.interrupt();
+        }
+
+        secondsElapsed.set(0);
+        isGameRunning = true;
     }
 
     private String getColoredMineCount(int minesNearby) {
